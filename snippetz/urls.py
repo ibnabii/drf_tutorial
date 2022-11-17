25001ce6eae561df1259b1a5dd87edf09172addc
ibnabii <bla@bla.pl>
2022-11-17 21:59:05 +0100

    Refactored to use class based views

--- snippetz/urls.py
+++ snippetz/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippetz import views
 
+# v3
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.snippet_list.as_view()),
+    path('snippets/<int:pk>/', views.snippet_detail.as_view()),
 ]
+
+# # v1
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+# ]
+# v 2
+urlpatterns = format_suffix_patterns(urlpatterns)
